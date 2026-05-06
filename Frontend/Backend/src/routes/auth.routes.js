15a122571674e74a5276baf5f5c442bcf2359950
iamkrn <karanpartaps27@gmail.com>
2026-05-06 12:21:24 +0530

    feat: remove obsolete controllers, models, routes, and middlewares
    - Deleted application, auth, chat, message, dashboard, document, student, user controllers.
    - Removed associated models for application, chat, message, document, student, and user.
    - Eliminated routes for application, auth, chat, message, dashboard, document, student, and user.
    - Removed middlewares for authentication, role checking, and file upload handling.
    - Cleaned up socket handling code related to chat functionality.

--- Frontend/Backend/src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const express =  require('express');
-const { LoginUser, registerUser } = require('../controllers/auth.controller');
-const router = express.Router()
-
-router.post('/login', LoginUser);
-router.post('/register',registerUser)
-
-module.exports = router;
