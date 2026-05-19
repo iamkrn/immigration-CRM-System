6a6dd8537729753e11fb5367c041248aa70a0140
iamkrn <karanpartaps27@gmail.com>
2026-05-19 14:22:30 +0530

    feat: update Firebase initialization to use environment variables

--- Frontend/public/firebase-messaging-sw.js
+++ Frontend/public/firebase-messaging-sw.js
@@ -3,12 +3,12 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyAi5V6bswE873Wsz1iSH8XwNoIYi_mEpv0",
-  authDomain: "college-review-crm.firebaseapp.com",
-  projectId: "college-review-crm",
-  storageBucket: "college-review-crm.firebasestorage.app",
-  messagingSenderId: "685472359680",
-  appId: "1:685472359680:web:cd70f0cf0d0cd0ebdc3fe5",
+ apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 });
 
 const messaging = firebase.messaging();
